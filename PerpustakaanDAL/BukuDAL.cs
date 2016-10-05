16aeec63f762a6efcb6ee5d4e94c779eedd38ae7
acilart <acildissident@gmail.com>
2016-10-05 13:14:20 +0700

    nambah reference entitiy framework

--- PerpustakaanDAL/BukuDAL.cs
+++ PerpustakaanDAL/BukuDAL.cs
@@ -4,10 +4,18 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using PerpustakaanModel;
+using System.Data.Entity;
 namespace PerpustakaanDAL
 {
     public class BukuDAL
     {
+        public List<MstBuku> GetAllBuku()
+        {
+            var result = new List<MstBuku>();
+            using(var db = new PerpustakaanDbContext()){
 
+            } 
+            return result;
+        }
     }
 }
